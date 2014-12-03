9be335ae0073e1baecf90cf05e2fb2a72859c5f5
yanglujuncc <yanglujuncc@gmail.com>
2014-12-03 16:45:05 +0800

    second commit

--- src/main/java/ylj/TestMQTT/SyncPublisher.java
+++ src/main/java/ylj/TestMQTT/SyncPublisher.java
@@ -31,7 +31,7 @@ class SyncPublisher {
 
 		String user = env("APOLLO_USER", "admin");
 		String password = env("APOLLO_PASSWORD", "password");
-		String host = env("APOLLO_HOST", "ssl://localhost");
+		String host = env("APOLLO_HOST", "localhost");
 		int port = Integer.parseInt(env("APOLLO_PORT", "61613"));
 		final String destination = arg(args, 0, "/topic/event");
 
