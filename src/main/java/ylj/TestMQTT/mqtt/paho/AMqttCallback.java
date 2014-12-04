692d1bff80b60e8e96b7553ebe5ec3501e79f292
yanglujuncc <yanglujuncc@gmail.com>
2014-12-04 17:59:22 +0800

    add callback

---
+++ src/main/java/ylj/TestMQTT/mqtt/paho/AMqttCallback.java
@@ -0,0 +1,27 @@
+package ylj.TestMQTT.mqtt.paho;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class AMqttCallback implements MqttCallback{
+
+	@Override
+	public void connectionLost(Throwable arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deliveryComplete(IMqttDeliveryToken arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void messageArrived(String arg0, MqttMessage arg1) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
